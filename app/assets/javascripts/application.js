8c352b29017e396d0b2c7eb7fe4127ccfe0317b7
Kaoru Kohashigawa <dev@kaoruk.com>
2016-05-06 16:16:52 -0700

    setup js to create and update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,103 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require backbone
+
+var $ = window.$;
+var _ = window._;
+var Backbone = window.Backbone;
+
+$(function () {
+  _.templateSettings = {
+    interpolate: /\{\{\=(.+?)\}\}/g,
+    evaluate: /\{\{(.+?)\}\}/g,
+  };
+
+  var UserModel = Backbone.Model.extend({});
+
+  var UserCollection = Backbone.Collection.extend({
+    url: '/api/users',
+    model: UserModel,
+  });
+
+  var UserView = Backbone.View.extend({
+    events: {
+      'submit': 'updateUser',
+    },
+
+    template: _.template($('#user-template').html()),
+    render: function () {
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+    updateUser: function (e) {
+      e.preventDefault();
+      var self = this;
+      var username = $(e.target).find('input').val();
+
+      $.ajax({
+        method: 'PUT',
+        url: '/api/users/' + self.model.get('id') + '.json',
+        data: {
+          users: { username: username },
+        },
+      }).done(function () {
+        self.model.set({ username: username });
+      });
+    },
+  });
+
+  var UsersList = Backbone.View.extend({
+    initialize: function () {
+      this.children = [];
+      this.listenTo(this.collection, 'sync add remove', this.render);
+    },
+
+    render: function () {
+      this.removeChildren();
+
+      var html = _.map(this.collection.models, function (model) {
+        return new UserView({
+          model: model,
+        }).render().el;
+      }, this);
+
+      this.$el.html(html);
+
+      return this;
+    },
+
+    removeChildren: function () {
+      this.children.forEach(function (view) {
+        view.remove();
+      });
+    },
+
+    remove: function () {
+      this.removeChildren();
+      Backbone.View.prototype.remove.apply(this, arguments);
+    },
+  });
+
+  var users = new UserCollection();
+
+  new UsersList({
+    el: '.list-of-users',
+    collection: users,
+  });
+
+  users.fetch();
+
+  $('.new-user-form').on('submit', function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: '/api/users',
+      method: 'POST',
+      data: {
+        users: {
+          username: $(e.target).find('input').val(),
+        },
+      },
+    }).done(users.add.bind(users));
+  });
+});
